3ceeb336e782ff29570c9edaf0746b0723f3fda3
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-02-26 16:55:11 -0300

    Fim aula

--- meusite/urls.py
+++ meusite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('sobre/', include('sobre.urls')),
